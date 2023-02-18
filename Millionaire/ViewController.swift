fbcdb974ccbbb12cf94c2a19a516fabb3507828f
Semen Vinnikov <semvinnikov@yandex.ru>
2023-02-18 13:06:19 +0300

    Initial Commit

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Семён Винников on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
